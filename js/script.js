35e0e5c442328a652a2702c9271aae9f51e31b62
Aleksandr Yakimtsev <a.yakimtsev@sana-commerce.com>
2018-03-14 14:44:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var $contentBox = $('#pbContentFlow'),
+    templates = {
+        layout: $('#pbTmpLayout').html().trim(),
+        text: $('#pbTmpText').html().trim(),
+        image: $('#pbTmpImage').html().trim(),
+        columns: $('#pbTmpColumns').html().trim(),
+        'edit-panel': $('#pbTmpColumns').html().trim(),
+    };
+
+
+function initDropable(){
+    $('.pb-layout:not(.ui-ui-droppable)').droppable({
+        greedy: true,
+        accept:'.pb-block',
+        tolerance: 'pointer',
+        drop: function( event, ui ) {
+            var blockType = ui.draggable.attr('data-type'),
+                $target = $(event.target);
+
+            $target.parents('.pb-layout').droppable("disable");
+            
+            $target.droppable("disable")
+                .addClass('state-dropped')
+                .append(templates[blockType]);
+
+            $contentBox.append(templates['layout']);
+            initDropable();
+        }
+    });
+}
+
+initDropable();
+
+
+$('.pb-block').each(function(){
+    var $ths = $(this);
+
+    $ths.draggable({
+        revert: true,
+        revertDuration: 0,
+        opacity: 0.35
+    });
+});
