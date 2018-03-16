6bdd12199a1f33da27a98100b8e23d63e9c7ba06
Aleksandr Yakimtsev <a.yakimtsev@sana-commerce.com>
2018-03-16 14:44:26 +0200

    update delete functionality

--- js/script.js
+++ js/script.js
@@ -39,16 +39,44 @@ var previewsCallbacks = {
 
             switch (type){
                 case 'edit':
-
+                    console.log('edit init');
                 break;
                 case 'delete':
+                    $ths.click(function(e){
+                        e.preventDefault();
+                        if($ths.closest('.pb-inner-column').length){
+                            $ths.closest('.pb-inner-column')
+                                    .removeClass('state-dropped ui-droppable-active')
+                                    .droppable("enable");
+                        } else {
+                            $ths.parents('.pb-layout') 
+                                    .removeClass('state-dropped ui-droppable-active')
+                                    .droppable("enable");
 
+
+                            $ths.closest('.pb-layout').add($ths.closest('.pb-layout').children('.pb-layout'))
+                                    .removeClass('state-dropped ui-droppable-active')
+                                    .droppable("enable");
+                                    
+                        }
+                        $ths.closest('.pb-preview').remove();
+                    });
                 break;
                 case 'move':
                     $ths.draggable({
+                        start: function( event, ui ) {
+                            //$contentBox.addClass("state-hide-editPanels");
+                            //debugger;
+                            $(ui.helper).closest('.pb-layout[data-type="full"]').css('z-index', 9999);
+                        },
+                        stop: function( event, ui ) {
+                            //$contentBox.removeClass("state-hide-editPanels");
+                            $(ui.prevObject).closest('.pb-layout[data-type="full"]').css('z-index', '');
+                        },
+                        // helper: "clone",
                         revert: true,
                         revertDuration: 0,
-                        opacity: 0.35
+                        opacity: 1
                     });
                 break;
             }
